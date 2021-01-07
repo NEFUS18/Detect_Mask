56614d9106dbc56afc865448104fe44a4222b814
kbh1860 <kbh10211@gmail.com>
2021-01-08 02:55:57 +0900

    이 사람 마-스크 안썼어요

---
+++ PushingBoxAPI.py
@@ -0,0 +1,24 @@
+# import urllib
+# import urllib.request as urllib2
+#
+# class pushingbox():
+#     url = ""
+#
+#     def __init__(self, key):
+#         url = 'http://api.pushingbox.com/pushingbox'
+#         values = {'devid': key}
+#
+#         try:
+#             data = urllib.urlencode(values)
+#             req = urllib2.Request(url, data)
+#             sendrequest = urllib2.urlopen(req)
+#         except Exception as detail:
+#             print ("Error ", detail)
+#
+# import Pushingbox
+#
+# key = "v2515752DA7184F1"
+#
+# pushingbox(key)
+
+print("Hello World")
